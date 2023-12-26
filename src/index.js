26e075a2ee1f67224ccf5acfda1bf4b747001176
Sergey Kazhaev <surakij6@gmail.com>
2023-12-26 23:32:35 +0400

    css loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { sum } from "./sum";
+import "./css/style.css";
 
-const h1 = document.body.getElementsByTagName("h1");
+const h1 = document.body.querySelector("h1");
 h1.innerText = sum(5, 6);
